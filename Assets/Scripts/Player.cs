596b797d9c57f18d6feaea30ea5b56eb3c0643b0
15-JiaJie <155930454+15-JiaJie@users.noreply.github.com>
2024-01-23 15:46:36 +0800

    done with lab 10

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    // Scores
+    public Text ScoreText;
+    private int Score;
+
+
+
+    public float speed;
+
+    private float MaxY = 3.9f;
+    private float MinY = -3.8f;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rb.IsSleeping())
+        {
+            rb.WakeUp();
+        }
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        transform.position = new Vector3(-12.24f, Mathf.Clamp(transform.position.y, MinY, MaxY), 0);
+
+        ScoreText.GetComponent<Text>().text = "Score: " + Score;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            
+            SceneManager.LoadScene("GameOver");
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Scoring")
+        {
+            Score += 1;
+        }
+    }
+}
---
+++ Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject[] SpawnObject;
+    float PositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+    }
+    
+
+    void SpawnObjects()
+    {
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+
+        int prefab_num = Random.Range(0, 3);
+        Instantiate(SpawnObject[prefab_num], transform.position, transform.rotation);
+    }
+}
